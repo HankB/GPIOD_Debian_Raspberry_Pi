00c5830978c3f6ca6146cd01b39d35c32e71ba52
Hank Barta <hbarta@gmail.com>
2023-01-27 21:40:13 -0600

    read a GPIO

---
+++ C_blinky/README.md
@@ -0,0 +1,9 @@
+# C Blinky
+
+Produce the enbedded version of "Hello World" using C and he `gpiod` library to deal with GPIO pins as inputs and outputs.
+
+Platform is Raspberry Pi 3B+ running off an SSD on Debian Bullseye (Not Raspberry Pi OS.) 
+
+## `read`
+
+Read the state of GPIO 7.
---
+++ C_blinky/read.c
@@ -0,0 +1,20 @@
+/*
+* Read the state of GPIO 7 on a Raspberry Pi
+
+Build:
+   gcc -Wall -o read read.c -l gpiod
+*/
+
+#include <stdio.h>
+#include <gpiod.h>
+
+const unsigned int gpio = 7;
+
+int main(int argc, char **argv)
+{
+    int rc = gpiod_ctxless_get_value("/dev/gpiochip0",
+                                     gpio, false, "consumer");
+    if (rc < 0)
+        perror("gpiod_ctxless_get_value");
+    printf("Hello World GPIO %d is %d\n", gpio, rc);
+}
