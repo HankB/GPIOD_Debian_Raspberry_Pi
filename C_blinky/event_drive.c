7165bf102f32d2bb0274ad6abaddc084e2473097
Hank Barta <hbarta@gmail.com>
2024-06-19 14:20:07 -0500

    switch GPIO, follow input

--- C_blinky/event_drive.c
+++ C_blinky/event_drive.c
@@ -10,25 +10,19 @@ Build:
 #include <unistd.h>
 #include <gpiod.h>
 
-const unsigned int gpio_read = 7;
+const unsigned int gpio_read = 20;
 const unsigned int gpio_drive = 8;
-const struct timespec timeout = {10, 0}; // 10s
+const struct timespec timeout = {10, 0}; // run for 10s and exit
 
-// callback to keep output on for 1/2 s
-void drive_cb(void *unused)
-{
-    usleep(500000);
-}
-
-// callback to drive the output on rising edge
+// callback to drive the output on rising/falling edge
 int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused)
 {
     printf("i: %d, j:%d\n", i, j);
     if (j == 0)
         return 1;
     int rc = gpiod_ctxless_set_value("/dev/gpiochip0",
-                                     gpio_drive, 1, false,
-                                     "consumer", drive_cb, 0);
+                                     gpio_drive, i-2, false,
+                                     "consumer", 0 , 0);
     if (rc < 0)
     {
         perror("gpiod_ctxless_get_value");
@@ -41,7 +35,7 @@ int event_cb(int i, unsigned int j, const struct timespec *ts, void *unused)
 int main(int argc, char **argv)
 {
     int rc = gpiod_ctxless_event_monitor("/dev/gpiochip0",
-                                         GPIOD_CTXLESS_EVENT_RISING_EDGE,
+                                         GPIOD_CTXLESS_EVENT_BOTH_EDGES,
                                          gpio_read, false, "consumer",
                                          &timeout, 0, event_cb, 0);
     if (rc < 0)
